18ea3f4acd55b7146dd66ad6dbcfa86ea11c1e0a
Paige Sun <paige.sun.dev@gmail.com>
2020-05-24 00:25:46 -0700

    Create new MacOS project

---
+++ iOSLogConsole/iOSLogConsole/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSLogConsole
+//
+//  Created by Paige Sun on 2020-05-24.
+//  Copyright © 2020 Paige Sun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ iOSLogConsole/iOSLogConsole/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSLogConsole
+//
+//  Created by Paige Sun on 2020-05-24.
+//  Copyright © 2020 Paige Sun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
