ad6d348b9d956a755e06fb146e80f195272b957e
Paige Sun <paige.sun.dev@gmail.com>
2022-01-16 10:50:12 -0800

    fix: Improve performance by 1) Get more data from file at a time, 2) Increase FileReader POS, 3) Don't add more to the FileReader queue if File is already being read.

--- RainbowLogger/RainbowLogger/FileReader/FileReader.m
+++ RainbowLogger/RainbowLogger/FileReader/FileReader.m
@@ -14,6 +14,7 @@
   NSFileHandle* fileHandle_;
   dispatch_queue_t fileReaderQueue_;
   NSString *lastLine_;
+  BOOL isRunning_;
 }
 
 - (void)dealloc {
@@ -27,13 +28,13 @@
 - (id)init {
   self = [super init];
   if (self != nil) {
-    dispatch_queue_attr_t qos = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_BACKGROUND, -1);
+    dispatch_queue_attr_t qos = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_UTILITY, -1);
     fileReaderQueue_ = dispatch_queue_create("fileReaderQueue", qos);
   }
   
   [self _readLines];
   
-  [NSTimer scheduledTimerWithTimeInterval:0.3
+  [NSTimer scheduledTimerWithTimeInterval:0.05
                                    target:self
                                  selector:@selector(_onTick:)
                                  userInfo:nil
@@ -87,32 +88,75 @@
   [self _readLines];
 }
 
+/*
+ Get data from file an arbitary # of times before batching new lines to the delegate.
+ 
+ Useful for when there are a lot of logs in the file.
+ If maxDataCount is too low, logs displayed may lag behind logs in the file.
+ If maxDataCount is too high, logs displayed may stutter as each new chunk logs is too big to process.
+ 
+ Manual testing to output a lot of logs for "Edit Script":
+ ```
+ function echoLogs {
+     for (( c=1; c<=100000; c++ ))
+        do
+           echo "Welcome $c times"
+        done
+ }
+ echoLogs
+ ```
+ **/
 -(void)_readLines {
   __weak __typeof__(self) weakSelf = self;
+  if (isRunning_) {
+    return;
+  }
   dispatch_async(fileReaderQueue_, ^{
     __strong __typeof(weakSelf) strongSelf = weakSelf;
     if (!strongSelf) {
       return;
     }
+    strongSelf->isRunning_ = YES;
+    
+    int dataCount = 0;
+    int maxDataCount = 20;
+    
     NSData *data = [strongSelf->fileHandle_ availableData];
-    if (data.length > 0) {
+    NSMutableArray *allLines = [[NSMutableArray alloc] init];
+
+    while (data.length > 0) {
       NSString *longString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-      if (longString) {
-        [strongSelf _parseLinesFromString:longString];
+      if (!longString) {
+        return;
+      }
+      NSArray *newLines = [strongSelf _parseLinesFromString:longString];
+      [allLines addObjectsFromArray:newLines];
+      if (dataCount < maxDataCount) {
+        dataCount++;
+        data = [strongSelf->fileHandle_ availableData];
+      } else {
+        data = [[NSData alloc] init];
       }
     }
+
+    if (allLines.count > 0) {
+      [strongSelf.delegate fileReaderDidReadLines:allLines];
+    }
+    
+    strongSelf->isRunning_ = NO;
   });
 }
 
-- (void)_parseLinesFromString:(NSString *)concatenatedLines {
+- (NSArray*)_parseLinesFromString:(NSString *)concatenatedLines {
   NSString *newConcatenatedLines = lastLine_ ? [lastLine_ stringByAppendingString:concatenatedLines] : concatenatedLines;
   NSArray *linesArray = [newConcatenatedLines componentsSeparatedByCharactersInSet:NSCharacterSet.newlineCharacterSet];
   if (linesArray.count > 0) {
     // The last object is often half a line, so append it to the beginning of the next concatenatedLines
     lastLine_ = linesArray.lastObject;
     NSArray *removingLast = [linesArray subarrayWithRange:NSMakeRange(0, linesArray.count - 1)];
-    [self.delegate fileReaderDidReadLines:removingLast];
+    return removingLast;
   }
+  return nil;
 }
 
 @end
